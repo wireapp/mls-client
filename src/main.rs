5d24863bc4933c7d442baceff85df83849116bd4
Artyom Kazak <yom@artyom.me>
2018-10-30 16:11:29 +0100

    Interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate rhai;
+
+use rhai::{Engine, RegisterFn, Scope};
+use std::io::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut engine = Engine::new();
+    let mut scope = Scope::new();
+
+    engine.register_fn("send", append_blob);
+    engine.register_fn("recv", get_blobs);
+
+    loop {
+        print!("> ");
+        let mut input = String::new();
+        stdout().flush().expect("couldn't flush stdout");
+        if let Err(e) = stdin().read_line(&mut input) {
+            println!("input error: {}", e);
+        }
+
+        if let Err(e) = engine.consume_with_scope(&mut scope, &input) {
+            println!("error: {}", e);
+        }
+    }
+}
+
+/// Store a blob for a specific group.
+pub fn append_blob(group_id: String, blob: String) {
+    println!("{} <>= {}", group_id, blob)
+}
+
+/// Receive all blobs for a specific groups.
+pub fn get_blobs(group_id: String) {
+    println!("{}...", group_id)
 }
