3cec996c9f1528a3ca2ef878375e1b512cbf1ad1
Artyom Kazak <yom@artyom.me>
2018-11-03 00:49:43 +0100

    Abstract out serialization

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,6 @@
 use melissa::codec::Codec;
+use serde::Deserialize;
+use serde::{de, ser};
 use std::fs;
 use std::io;
 use std::path::Path;
@@ -16,4 +18,28 @@ pub fn write_codec<P: AsRef<Path>, T: Codec>(
     value: &T,
 ) -> io::Result<()> {
     fs::write(path, Codec::encode_detached(value).as_slice())
+}
+
+/// Implement `Serialize` with `Codec`.
+pub fn serialize_codec<C, S>(
+    value: &C,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: ser::Serializer,
+    C: Codec,
+{
+    serializer.serialize_bytes(value.encode_detached().as_slice())
+}
+
+/// Implement `Deserialize` with `Codec`.
+pub fn deserialize_codec<'de, C, D>(deserializer: D) -> Result<C, D::Error>
+where
+    D: de::Deserializer<'de>,
+    C: Codec,
+{
+    Vec::deserialize(deserializer).and_then(|v| {
+        C::decode_detached(v.as_slice())
+            .map_err(|_| de::Error::custom("Failed to decode"))
+    })
 }
